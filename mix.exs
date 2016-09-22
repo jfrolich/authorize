02a60921539b5f8c279f28148c7ce137e9c1c98c
Jaap Frolich <jaap@she-corp.com>
2016-09-22 23:42:40 +0800

    Add package metadata

--- mix.exs
+++ mix.exs
@@ -9,7 +9,8 @@ defmodule Gatekeeper.Mixfile do
       elixir: "~> 1.3",
       build_embedded: Mix.env == :prod,
       start_permanent: Mix.env == :prod,
-      deps: deps()
+      deps: deps(),
+      package: package(),
     ]
   end
 
